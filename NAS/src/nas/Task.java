32ce2d7fefefe5e9e27bcdf495c11ebd17f0a18e
pezhuzha <144140918+pezhuzha@users.noreply.github.com>
2024-11-05 11:41:26 +0100

    Basic send/remove

--- NAS/src/nas/Task.java
+++ NAS/src/nas/Task.java
@@ -1,54 +1,136 @@
 package nas;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.*;
 import java.net.Socket;
+
 public class Task implements Runnable {
 	private Socket s;
 	private File root;
-	Task (Socket soc,File ro){
-		this.s=soc;
-		this.root=ro;
+
+	Task(Socket soc, File ro) {
+		this.s = soc;
+		this.root = ro;
 	}
+
 	public void run() {
 		String res=null;
-		File aux[]=null,subdir=null;
+		String path=null;
+		File aux[]=null;
+		File subdir=null;
 		try(InputStream is=s.getInputStream();
-			OutputStream os=s.getOutputStream();
-			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
-			BufferedReader br=new BufferedReader(new InputStreamReader(is,"UTF-8")))
+				OutputStream os=s.getOutputStream();
+				BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+				BufferedReader br=new BufferedReader(new InputStreamReader(is,"UTF-8")))
 		{
-			res=br.readLine();
-			switch(res.charAt(0)) {
-			case 'm'://mkdir
-				break;
-			case 's'://send
-				break;
-			case 'r'://rm
-				break;
-			case 'l'://ls
+			do {
 				res=br.readLine();
-				if(res.isBlank()) {
-				aux=root.listFiles();}
-				else {
-					subdir=new File(root.getAbsolutePath()+File.separator+res);
-					aux=subdir.listFiles();
-				}
-				bw.write(aux.length+"\n");
-				for(int i=0;i<aux.length;i++) {
-					if(aux[i].isDirectory()) {
-						bw.write(aux[i].getName()+File.separator+"\n");
+				if (res!=null) {
+					switch(res.charAt(0)) {
+					case 'm'://mkdir
+						res=br.readLine();
+						path=root.getAbsolutePath()+File.separator+res;
+						if(new File(path).mkdirs()) {
+							bw.write("OK\n");
+						}
+						else {
+							bw.write("BAD\n");
+						}
+						bw.flush();
+
+						break;
+					case 's'://sended to server
+						res=br.readLine();
+						path=root.getAbsolutePath()+File.separator+res;
+						File comprobarRuta=new File(path);
+						if(comprobarRuta.exists()) {
+							bw.write("EXISTS\n");
+							bw.flush();
+						}else{
+							bw.write("OK\n");
+							bw.flush();
+							FileOutputStream archivorecibido=new FileOutputStream(comprobarRuta);
+							int ended=is.read();
+							while(ended!=-1) {
+								archivorecibido.write(ended);
+								ended=is.read();
+							}
+							archivorecibido.flush();
+							archivorecibido.close();
+							bw.write("OK \n");
+							bw.flush();
+						}
+						break;
+					case 'r'://rm
+						res=br.readLine();
+						path=root.getAbsolutePath()+File.separator+res;
+						
+						File delete=new File(path);
+						if(delete.exists()) {
+							if(delete.delete()) {
+								bw.write("OK \n");
+								bw.flush();
+							}else {
+								bw.write("BAD \n");
+								bw.flush();
+							}
+						}
+						else {
+							bw.write("NOEXISTS \n");
+							bw.flush();
+						}
+						
+						break;
+					case 'l'://ls
+						res=br.readLine();
+						if(res.isBlank()) //caso root
+						{
+							aux=root.listFiles();
+						}
+						else if(res.contains("..")) //no dejar que acceda a directorios superiores
+						{
+							bw.write("UNREADABLE\n");
+							bw.flush();
+						}
+						else //normal
+						{
+							path=root.getAbsolutePath()+File.separator+res;
+							subdir=new File(path);
+							if(subdir!=null && subdir.canRead() && subdir.isDirectory()) {
+								aux=subdir.listFiles();}
+							else {//caso de leer algo que no es directorio o no se pude leer por permisos
+								bw.write("BAD\n");
+								bw.flush();
+							}
+						}
+						if(aux!=null) {
+							bw.write("OK\n");
+							bw.flush();
+
+							//listar todos los contenidos del directorio pedido
+							for(int i=0;i<aux.length;i++) {
+								if(aux[i].isDirectory()) {
+									bw.write(aux[i].getName()+File.separator+"\n");
+								}
+								else {
+									bw.write(aux[i].getName()+"\n");
+								}
+							}
+							bw.flush();
+						}
+						else {
+							bw.write("BAD\n");
+							bw.flush();
+						}
+						break;
+
 					}
-					else {
-						bw.write(aux[i].getName()+"\n");
-					}
-					bw.flush();
 				}
-				break;
 			}
-			
-			
-			
+			while(!s.isClosed());
+
+
 		}
 		catch(IOException e1) {
 			e1.printStackTrace();
@@ -61,12 +143,5 @@ public class Task implements Runnable {
 				e.printStackTrace();
 			}
 		}
-
-		
-		
-		
-		
-		
-		
 	}
 }
--- NAS/src/nas_client/NAS_client.java
+++ NAS/src/nas_client/NAS_client.java
@@ -2,6 +2,7 @@ package nas_client;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,89 +16,137 @@ public class NAS_client {
 		String in=null,name=null,resp=null;
 		int selected;
 		try (Socket s=new Socket("127.0.0.1",55555);
-			InputStream is=s.getInputStream();
-			OutputStream os=s.getOutputStream();
-			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
-			BufferedReader br=new BufferedReader(new InputStreamReader(is,"UTF-8"));
-			BufferedReader sysin=new BufferedReader(new InputStreamReader(System.in)))
+				InputStream is=s.getInputStream();
+				OutputStream os=s.getOutputStream();
+				BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+				BufferedReader br=new BufferedReader(new InputStreamReader(is,"UTF-8"));
+				BufferedReader sysin=new BufferedReader(new InputStreamReader(System.in)))
 		{	
 			do {
-			System.out.println("Menu:\n"
-					+ "		1:Crear directorio\n"
-					+ "		2:Enviar archivo\n"
-					+ "		3:Eliminar archivo\n"
-					+ "		4:Listar archivos\n"
-					+ "		9:Salir\n");
-			in=sysin.readLine();
-			selected=parsetoint(in);
-			switch(selected) {
-			case 0:
-				System.out.println("Error no has insertado un número");
-				break;
-			case 1:
-				
-				System.out.println("Nombre del directorio a crear");
-				
-				bw.write("m\n");//mkdir
-				bw.flush();
-				
-				name=sysin.readLine();
-				
-				bw.write(name+"\n");
-				bw.flush();
-				
-				resp=br.readLine();
-				
-				if(resp=="OK") {
-					System.out.println("El directorio se ha creado");
-				}
-				else if(resp=="BAD") {
-					System.out.println("Error el directorio no se ha podido crear");
-				}
-				break;
-			case 2:
-				System.out.println("Nombre del archivo a enviar");
-				
-				bw.write("s\n");//send
-				bw.flush();
-				
-				
-				break;
-			case 3:
-				System.out.println("Nombre del archivo a eliminar");
-				
-				bw.write("r\n");//rm
-				bw.flush();
-				
-				break;
-			case 4:
-				System.out.println("Nombre del directorio a listar(Enter si es root)");
-				
-				bw.write("l\n");//ls
-				bw.flush();
-				
-				name=sysin.readLine();
-				
-				bw.write(name+"\n");
-				bw.flush();
-				
-				resp=br.readLine();
-				int length=parsetoint(resp);
-				
-				for(int i=0;i<length;i++) {
-					System.out.println(br.readLine());
-				}
+				System.out.println("Menu:\n"
+						+ "		1:Crear directorio\n"
+						+ "		2:Enviar archivo\n"
+						+ "		3:Eliminar archivo\n"
+						+ "		4:Listar archivos\n"
+						+ "		9:Salir\n");
+				in=sysin.readLine();
+				selected=parsetoint(in);
+				switch(selected) {
+				case 1:
+					System.out.println("Nombre del directorio a crear");
 
-				
-				break;
-			}
+					bw.write("m\n");//mkdir
+					bw.flush();
+
+					name=sysin.readLine();
+
+					bw.write(name+"\n");
+					bw.flush();
+
+					resp=br.readLine();
+
+					if(resp.equalsIgnoreCase("OK")) {
+						System.out.println("El directorio se ha creado");
+					}
+					else if(resp.equalsIgnoreCase("BAD")) {
+						System.out.println("Error, el directorio no se ha podido crear");
+					}
+					break;
+				case 2:
+					System.out.println("Nombre del archivo a enviar");
+
+					bw.write("s\n");//send to server
+					bw.flush();
+
+					name=sysin.readLine();
+					File f=new File(name);
+					if(f.isFile()) {
+						String fileshortname[]=name.split(File.separator);
+						String filename=fileshortname[fileshortname.length-1];//nombre corto para no enviar toda la ruta absoluta del ordenador
+						bw.write(filename+"\n");
+						resp=br.readLine();
+						if(resp.equalsIgnoreCase("OK")) {//si no existe ningun archivo en el servidor con el mismo nombre
+							try(FileInputStream fos=new FileInputStream(f);){
+								byte b[]=fos.readAllBytes();
+								os.write(b);
+								os.flush();
+							}
+							catch(IOException ei1) {
+								ei1.printStackTrace();
+							}
+							if(resp.equalsIgnoreCase("OK")) {
+								System.out.println("Archivo creado exitosamente");
+							}
+						}
+						else if(resp.equalsIgnoreCase("EXISTS")){//existe un archivo con el mismo nombre 
+							System.out.println("El archivo ya existe (Es necesario borralo primero)");
+						}
+					}
+					else {
+						System.out.println("Error, no se puede enviar un directorio");}
+
+
+					break;
+				case 3:
+					System.out.println("Nombre del archivo/directorio a eliminar");
+
+					bw.write("r\n");//rm
+					bw.flush();
+					
+					name=sysin.readLine();
+					
+					bw.write(name+"\n");
+					bw.flush();
+					
+					if(resp.equalsIgnoreCase("OK")) {
+						System.out.println("Archivo borrado correctamente\n");
+						}
+					else if(resp.equalsIgnoreCase("BAD")) {
+						System.out.println("No se puede borrar el archivo\nSi es un directorio tiene que estar vacio\n");
+					}
+					else if(resp.equalsIgnoreCase("NOEXISTS")) {
+						System.out.println("No se puede borrar el archivo, no existe\n");
+					}
+					
+
+					break;
+				case 4:
+					System.out.println("Nombre del directorio a listar(Enter si es root)");
+
+					bw.write("l\n");//ls
+					bw.flush();
+
+					name=sysin.readLine();
+
+					bw.write(name+"\n");
+					bw.flush();
+
+					resp=br.readLine();
+					if(resp.equalsIgnoreCase("OK")) {
+						resp=br.readLine();
+						while(br.ready()){
+							System.out.println(br.readLine());
+						}
+						}
+					else if(resp.equalsIgnoreCase("BAD")) {
+						System.out.println("No existe este directorio\n");
+					}
+					else if(resp.equalsIgnoreCase("UNREADABLE")) {
+						System.out.println("No se puede acceder a \"..\"\n");
+					}
+					
+					break;
+				default:
+					System.out.println("Error no has insertado un número");
+					break;
+				}
 			}while(selected!=9);
-			
-			
-		} catch (IOException e) {
+			System.out.println("Programa terminado");
+			return;
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 	private static int parsetoint(String probint) {
 		int integer=0;
